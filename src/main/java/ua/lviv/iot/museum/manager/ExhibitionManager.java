1970c4fc314767ab1d9aa786f2f945efd7685675
Dima Antoniuk <dimaantoniuk09@gmail.com>
2019-02-20 21:39:54 +0200

    with maven and plugins

---
+++ src/main/java/ua/lviv/iot/museum/manager/ExhibitionManager.java
@@ -0,0 +1,17 @@
+package ua.lviv.iot.museum.manager;
+
+import ua.lviv.iot.museum.models.Exhibit;
+import ua.lviv.iot.museum.models.Topic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface ExhibitionManager {
+
+    List<Exhibit> exhibits = new ArrayList<Exhibit>();
+
+    List<Exhibit> findByTheme(List<Exhibit> listOfExhibits , Topic theme);
+    List<Exhibit> sortByAge(List<Exhibit> listOfExhibits , boolean reverse);
+    List<Exhibit> sortByTimeInCurrentExhibition(List<Exhibit> listOfExhibits , boolean reverse);
+
+}
