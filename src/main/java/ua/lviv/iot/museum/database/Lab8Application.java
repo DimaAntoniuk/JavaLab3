5ef58fbb22c829710e6ed37db2b42512907ef069
Dima Antoniuk <dimaantoniuk09@gmail.com>
2019-04-01 22:43:46 +0300

    creating Lab8Application and test

--- src/main/java/ua/lviv/iot/museum/database/Lab8Application.java
+++ src/main/java/ua/lviv/iot/museum/database/Lab8Application.java
@@ -1,4 +1,13 @@
 package ua.lviv.iot.museum.database;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Lab8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab8Application.class, args);
+    }
+
 }
